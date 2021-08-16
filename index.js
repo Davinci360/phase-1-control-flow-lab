040c3139f6da44603c2b9756c1d6b0dfab28943f
davinci360 <jasonbrowntime@gmail.com>
2021-08-16 14:32:08 -0400

    3 scuber passing

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-function scuberGreetingForFeet(number){
+function scuberGreetingForFeet(number)
+{
   let result;
-  /* const feet = undefined;
-let greeting;
-if (feet <= 400) {
-  greeting = "free ride";
-} else if (feet <= 2500) {
-  greeting = "30 bucks";
-} else {
-  greeting = "no can do";
-}
+    /* const feet = undefined;
+  let greeting;
+  if (feet <= 400) {
+    greeting = "free ride";
+    } else if (feet <= 2500) {
+      greeting = "30 bucks";
+      } else {
+        greeting = "no can do";
+        }
   }*/
-    if (number <= 400 ) {
-      result = "This one is on me!";
-  } else if (number <= 2500) {
-      result = "I will gladly take your thirty bucks.";
-  } else {
-      result = "No can do.";
-  return result;
-  }
+
+      if (number <= 400) {
+        return 'This one is on me!';
+    } else if (number <= 2500) {
+        return "I will gladly take your thirty bucks.";
+    } else {
+        return "No can do.";
+    return result;
+    }
 }
 
 function ternaryCheckCity(){
