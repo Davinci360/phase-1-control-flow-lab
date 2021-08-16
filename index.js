afc5dd10ffc196421f01fd27d5fb5a267baf6cdf
davinci360 <jasonbrowntime@gmail.com>
2021-08-16 13:23:29 -0400

    fixed errors 0 passing

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 function scuberGreetingForFeet(number){
-let result
-  if (number < 400) {
-  result = "This one is on me!";
-} else if (number >= 2000); {
-  result = "I will gladly take your thirty bucks."}
-return result
+  let result;
+  /* const time = new Date().getHours(28);
+  let greeting;
+  if (time < 10) {
+    greeting = "Good morning";
+  } else if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }*/
+    if (number < 400) {
+      result = "This one is on me!";
+  } else if (number >= 2000) {
+      result = "I will gladly take your thirty bucks.";
+  } else if (number > 2500) {
+      result = "No can do.";
+  return result;
+  }
 }
 
 function ternaryCheckCity(){
-  let result
+  // Write your code here!
 
   /* — Use a ternary operator to return the correct response
 based on the desired destination of the passenger.*/
