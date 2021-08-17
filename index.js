a8e3ed6192fad30ec507cfdfbfd9249ea531eae0
davinci360 <jasonbrowntime@gmail.com>
2021-08-17 03:40:25 -0400

    All passing

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function scuberGreetingForFeet(number)
-{
+function scuberGreetingForFeet(number){
   let result;
     /* const feet = undefined;
   let greeting;
@@ -15,20 +14,38 @@ function scuberGreetingForFeet(number)
       if (number <= 400) {
         return 'This one is on me!';
     } else if (number <= 2500) {
-        return "I will gladly take your thirty bucks.";
+      return "I will gladly take your thirty bucks.";
     } else {
-        return "No can do.";
-    return result;
+      return "No can do.";
     }
-}
+  }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
 
-  /* — Use a ternary operator to return the correct response
-based on the desired destination of the passenger.*/
-}
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+  /* ternaryCheckCity()
+      1) returns "Ok, sounds good." when the city is NYC
+      2) should return "No go." if the destination city is not NYC*/
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+     // 1) should return "Thank you so much." if the tip is generous
+     case 'generous':
+      return 'Thank you so much.';
+     // 2) should return "Thank you." if the tip is not as generous
+     case 'not as generous':
+      return 'Thank you.';
+      //3) should return "Bye." if anything else
+      default:
+        return 'Bye.';
+  }
 }
+/* This is how it works:
+
+The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+If there is no match, the default code block is executed. */
